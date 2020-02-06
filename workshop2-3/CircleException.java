7937529d33f85fb4dbe798864dbfb028b880d71c
Duc Tai Nguyen <dtnguyen22@myseneca.ca>
2020-02-05 19:50:49 -0500

    clean

--- workshop2-3/CircleException.java
+++
@@ -1,8 +0,0 @@
-package workshop2;
-
-public class CircleException extends Exception {
-	public CircleException(String message){
-
-		super(message);
-	}
-}
--- workshop2-3/Shape.java
+++
@@ -1,6 +0,0 @@
-package workshop2;
-
-@FunctionalInterface
-public interface Shape {
-	double perimeter();
-}
